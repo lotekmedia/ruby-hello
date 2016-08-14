3e72087ab32307a36e9746a8b11df253a96c9bae
lotekmedia <me@johnmann.org>
2016-08-13 20:53:45 -0400

    Layout Updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  
+  get  '/help',    to: 'static_pages#help', as: 'help'
+  get  '/about',   to: 'static_pages#about', as: 'about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
+
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'static_pages#home'
 end
