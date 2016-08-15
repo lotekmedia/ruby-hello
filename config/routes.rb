fea650c8d145c52f8df400ea8cc72a9b3df7a6f8
lotekmedia <me@johnmann.org>
2016-08-14 20:50:01 -0400

    Added login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#home'
   
   get  '/help',    to: 'static_pages#help', as: 'help'
   get  '/about',   to: 'static_pages#about', as: 'about'
-  get  '/contact', to: 'static_pages#contact'
+  get  '/contact', to: 'static_pages#contact', as: 'contact'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
+  resources :users
 
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
 end
